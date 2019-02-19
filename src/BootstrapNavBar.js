2ee35dfa111a44ab27b0898bdf5c4837b424f0cb
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-19 10:24:21 -0500

    added on click styling

--- src/BootstrapNavBar.js
+++ src/BootstrapNavBar.js
@@ -2,6 +2,19 @@ import React, {Component} from 'react'
 import {Link} from "react-router-dom";
 
 class BootstrapNavBar extends Component{
+	constructor(){
+		super();
+		this.state = {
+			activeLink : ""
+		}
+	}
+
+	setActiveLink(linkRoute){
+		this.setState({
+			activeLink: linkRoute
+		})
+	}
+
   render(){
     return(
 		<nav className="navbar navbar-default navbar-fixed-top">
@@ -10,11 +23,13 @@ class BootstrapNavBar extends Component{
 		      <a className="navbar-brand">WebSiteName</a>
 		    </div>
 		    <ul className="nav navbar-nav">
-		    	<li><Link to="/">Home</Link></li>
-		      <li><Link to="/about">About</Link></li>
-		      <li><Link to="/images">Images</Link></li>
-		      <li><Link to="/weather">Weather</Link></li>
-		      <li><Link to="/mayorMessage">Mayor's Message</Link></li>
+		    	<li><Link onClick={()=>{this.setActiveLink("/home")}}to="/">
+						<span className={this.state.activeLink =="/home" ? "active" : ""}>Home
+						</span></Link></li>
+		      <li><Link onClick={()=>{this.setActiveLink("/about")}} to="/about">About</Link></li>
+		      <li><Link onClick={()=>{this.setActiveLink("/images")}} to="/images">Images</Link></li>
+		      <li><Link onClick={()=>{this.setActiveLink("/weather")}} to="/weather">Weather</Link></li>
+		      <li><Link onClick={()=>{this.setActiveLink("/mayorMessage")}} to="/mayorMessage">Mayor's Message</Link></li>
 		    </ul>
 		  </div>
 		</nav>    
